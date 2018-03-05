09bde5ae7566c395c4fd10b16cd490692e12d202
mikeskysilk <36085617+mikeskysilk@users.noreply.github.com>
2018-03-05 01:28:00 -0800

    Improved backup logic with snapshots, added compression

--- pveBarque.py
+++ pveBarque.py
@@ -25,37 +25,45 @@ class Backup(Resource):
 	def post(self, vmid):
 		vmdisk = 'vm-{}-disk-1'.format(vmid)
 		timestamp = datetime.strftime(datetime.now(),"_%Y-%m-%d_%H-%M")
-		#cmd = cmd[:-1] #trim last item from list b/c cmd ends with newline char
-		#return {'result': cmd} #return output list as json
 		config_file = ""
 		config_target = "{}.conf".format(vmid)
-		print(config_file)
+		#get config file
 		for paths, dirs, files in os.walk('/etc/pve/nodes'):
 			if config_target in files:
 				config_file = os.path.join(paths, config_target)
 				print(config_file)
+		#catch if container does not exist
 		if len(config_file) == 0:
 			return "error, {} is invalid CTID".format(vmid), 400
+		#copy config file
 		config_dest = "".join([path, vmdisk, timestamp, ".conf"])
 		copyfile(config_file, config_dest)
-		dest = "".join([path, vmdisk, timestamp, ".img"])
-		args = ['rbd export --export-format 1 {} {}'.format(vmdisk, dest)]
+		#create snapshot for backup
+		cmd = subprocess.check_output('rbd snap create {}@barque'.format(vmdisk), shell=True)
+		#protect snapshot during backup
+		cmd = subprocess.check_output('rbd snap protect {}@barque'.format(vmdisk), shell=True)
+		#create compressed backup file from backup snapshot
+		dest = "".join([path, vmdisk, timestamp, ".lz4"])
+		args = ['rbd export --export-format 2 {}@barque - | lz4 -9 - {}'.format(vmdisk, dest)]
 		cmd = subprocess.check_output(args, shell=True)#.split('\n') #run command then convert output to list, splitting on newline
+		#unprotect barque snapshot
+		cmd = subprocess.check_output('rbd snap unprotect {}@barque'.format(vmdisk), shell=True)
+		#delete barque snapshot
+		cmd = subprocess.check_output('rbd snap rm {}@barque'.format(vmdisk), shell=True)
 		return {'Backup file': os.path.basename(dest), 'Config file': os.path.basename(config_dest)}, 201
+
 class Restore(Resource):
 	def post(self,vmid):
 		fileimg = ""
 		fileconf = ""
+		filename = os.path.splitext(request.args['file'])[0]
 		node = ""
 		config_file = ""
 		vmdisk = 'vm-{}-disk-1'.format(vmid)
 		#check if backup and config files exist
 		if 'file' in request.args:
 			response = ""
-			print(request.args['file'])
-			filename = os.path.splitext(request.args['file'])[0]
-			print(filename)
-			fileimg = "".join([path, filename, ".img"])
+			fileimg = "".join([path, filename, ".lz4"])
 			fileconf = "".join([path, filename, ".conf"])
 			if not os.path.isfile(fileimg) and not os.path.isfile(fileconf):
 				return {'error': "unable to proceed, backup file or config file (or both) does not exist"}, 400
@@ -82,12 +90,21 @@ class Restore(Resource):
 		#delete container storage
 		imgdel = subprocess.check_output("pvesh delete /nodes/{}/storage/rbd_ct/content/rbd_ct:{}".format(node, vmdisk), shell=True)
 		print(imgdel)
+		#extract lz4 compressed image file
+		filetarget = "".join([path, filename, ".img"])
+		uncompress = subprocess.check_output("lz4 -d {} {}".format(fileimg, filetarget), shell=True)
+		print(uncompress)
 		#import new image
-		rbdimp = subprocess.check_output("rbd import {} {}".format(fileimg, vmdisk), shell=True)
+		rbdimp = subprocess.check_output("rbd import --export-format 2 {} {}".format(filetarget, vmdisk), shell=True)
 		print(rbdimp)
-		#image attenuation for kernel params
-		imgatten = subprocess.check_output("rbd feature disable {} object-map fast-diff deep-flatten".format(vmdisk), shell=True)
-		print(imgatten)
+		#delete uncompressed image file
+		rmuncomp = subprocess.check_output("rm {}".format(filetarget), shell=True)
+		print(rmuncomp)
+		#delete barque snapshot
+		cmd = subprocess.check_output('rbd snap rm {}@barque'.format(vmdisk), shell=True)
+		#image attenuation for kernel params #Removed after switching to format 2
+		# imgatten = subprocess.check_output("rbd feature disable {} object-map fast-diff deep-flatten".format(vmdisk), shell=True)
+		# print(imgatten)
 		#replace config file
 		copyfile(fileconf, config_file)
 		#start container
@@ -100,7 +117,7 @@ class ListAllBackups(Resource):
 		confs = []
 		for paths, dirs, files in os.walk(path):
 			for f in files:
-				if not f.endswith('.conf'):
+				if f.endswith('.lz4'):
 					result.append(f)
 				elif f.endswith('.conf'):
 					confs.append(f)
@@ -108,7 +125,7 @@ class ListAllBackups(Resource):
 
 class ListBackups(Resource):
 	def get(self, vmid):
-		files = sorted(os.path.basename(f) for f in glob("".join([path, "vm-{}*.img".format(vmid)])))
+		files = sorted(os.path.basename(f) for f in glob("".join([path, "vm-{}*.lz4".format(vmid)])))
 		return {'backups': files}
 class DeleteBackup(Resource):
 	def post(self,vmid):
